5e349bd8930b4590284cfae53fc205bd387b42c0
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-14 07:38:51 +0700

    Добавьте файлы проекта.

---
+++ Ostrich.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birdsWithEggs
+{
+    internal class Ostrich : Bird
+    {
+        public override Egg[] LayEggs(int mumberOfEggs)
+        {
+            Egg[] eggs = new Egg[mumberOfEggs];
+            for (int i = 0; i < mumberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() + 12, "speckled");
+            }
+            return eggs;
+        }
+    }
+}
---
+++ Pigeon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birdsWithEggs
+{
+    internal class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int mumberOfEggs)
+        {
+            Egg[] eggs = new Egg[mumberOfEggs];
+            for (int i = 0;  i < mumberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            }
+            return eggs;
+        }
+    }
+}
